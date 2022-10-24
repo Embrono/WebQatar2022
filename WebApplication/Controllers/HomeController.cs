45c7fdbb8b44729242faedc3d248b9cbbca7b965
andre <andre@DESKTOP-CTCCANA>
2022-10-24 09:42:45 -0300

    MVC

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Mensaje = "Hola manu";
+            return View();
+        }
+
+        public  IActionResult Tabla()
+        {
+            return View();
+        }
+    }
+}
